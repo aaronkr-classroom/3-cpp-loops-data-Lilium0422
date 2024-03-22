e4c4ae60da1d025c4e4a101222a4477cee977e5e
Lilium0422 <a63789492@gmail.com>
2024-03-22 12:07:47 +0900

    Update chp2ex9.cpp

--- week3/chp2ex9.cpp
+++ week3/chp2ex9.cpp
@@ -3,6 +3,7 @@
 
 int main() {
 	int a, b;
+	std::cout << "비교할 2개의 숫자 입력 : ";
 	std::cin >> a >> b;
 	if (a > b) std::cout << a << " 이(가) " << b << "보다 더 큽니다." << std::endl;
 	else std::cout << b << " 이(가) " << a << "보다 더 큽니다." << std::endl;
